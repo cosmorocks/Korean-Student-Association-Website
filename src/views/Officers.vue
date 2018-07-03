d3cac7086545762c39771b98a01cc39cb1a415d3
Matthew Han <mlh6fc@virginia.edu>
2018-07-03 14:51:08 -0400

    GITHUB image problem is finally resolved, moved site hosting over to netlify because GitHub pages doesn't play well with this type of Vue application, started figuring out bootstrap grid again for the 'Officers' page.

--- src/views/Officers.vue
+++ src/views/Officers.vue
@@ -2,34 +2,34 @@
     <div class="officers container">
         <h1 class="meet-the-team-header">Meet the team</h1>
         <div class="people-holder container">
-            <div class="row">
-                <div class="person col-lg-3">
+            <div class="row justify-content-between">
+                <div class="col-xl-3 col-l-4 col-m-4 col-s-6">
                 </div>
-                <div class="person col-lg-3">
+                <div class="col-xl-3 col-l-4 col-m-4 col-s-6">
                 </div>
-                <div class="person col-lg-3">
+                <div class="col-xl-3 col-l-4 col-m-4 col-s-6">
                 </div>
-                <div class="person col-lg-3">
-                </div>
-            </div>
-            <div class="row">
-                <div class="person col-lg-3">
-                </div>
-                <div class="person col-lg-3">
-                </div>
-                <div class="person col-lg-3">
-                </div>
-                <div class="person col-lg-3">
+                <div class="col-xl-3 col-l-4 col-m-4 col-s-6">
                 </div>
             </div>
-            <div class="row">
-                <div class="person col-lg-3">
+            <div class="row justify-content-between">
+                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
                 </div>
-                <div class="person col-lg-3">
+                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
                 </div>
-                <div class="person col-lg-3">
+                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
                 </div>
-                <div class="person col-lg-3">
+                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
+                </div>
+            </div>
+            <div class="row justify-content-between">
+                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
+                </div>
+                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
+                </div>
+                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
+                </div>
+                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
                 </div>
             </div>
         </div>
@@ -46,14 +46,14 @@
         margin-top: 40px;
     }
     .people-holder{ /** strictly to help me build this page, delete before export **/
-        border: 2px dotted red;
+        /* border: 2px dotted red; */
         height: fit-content;
-        margin-top: 60px; 
     }
     .person{
         border: 1px solid blue;
         height: 250px;
         width: 200px;
         display: inline-block;
+        margin-top: 60px;
     }
 </style>
