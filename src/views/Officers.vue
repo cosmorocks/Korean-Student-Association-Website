c1decec1a3454c811cdf4ad232ace7a1dbc8ab16
MatthewLeeHan <mlh6fc@virginia.edu>
2018-07-07 18:39:55 -0400

    added SASS to project, trying to use CSS Grid on Officers Page along with bootstrap

--- src/views/Officers.vue
+++ src/views/Officers.vue
@@ -1,59 +1,37 @@
 <template>
     <div class="officers container">
         <h1 class="meet-the-team-header">Meet the team</h1>
-        <div class="people-holder container">
-            <div class="row justify-content-between">
-                <div class="col-xl-3 col-l-4 col-m-4 col-s-6">
-                </div>
-                <div class="col-xl-3 col-l-4 col-m-4 col-s-6">
-                </div>
-                <div class="col-xl-3 col-l-4 col-m-4 col-s-6">
-                </div>
-                <div class="col-xl-3 col-l-4 col-m-4 col-s-6">
-                </div>
+        <div class="people-holder">
+            <div class="person-holder">
+                
             </div>
-            <div class="row justify-content-between">
-                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
-                </div>
-                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
-                </div>
-                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
-                </div>
-                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
-                </div>
+            <div class="person-holder">
+
             </div>
-            <div class="row justify-content-between">
-                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
-                </div>
-                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
-                </div>
-                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
-                </div>
-                <div class="person col-xl-3 col-l-4 col-m-4 col-s-6">
-                </div>
+            <div class="person-holder">
+
             </div>
+            <div class="person-holder">
+
+            </div>
+
         </div>
     </div>
 </template>
 
-<style scoped>
-    .officers{
-        height: 1000px;
-    }
+<style lang="scss" scoped>
     .meet-the-team-header{
         text-align: center;
         font-size: 45px;
         margin-top: 40px;
     }
-    .people-holder{ /** strictly to help me build this page, delete before export **/
-        /* border: 2px dotted red; */
-        height: fit-content;
-    }
-    .person{
-        border: 1px solid blue;
-        height: 250px;
-        width: 200px;
-        display: inline-block;
-        margin-top: 60px;
+    .people-holder{
+        border: 1px solid red;
+        display: grid;
+        grid-template-columns: 4;
+        .person-holder{
+            border: 1px solid blue;
+            height: 200px;
+        }
     }
 </style>
