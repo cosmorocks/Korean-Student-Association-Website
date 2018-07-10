4b11ea267d9f0d8ba56f9214f3f5ceb3d0bf093e
MatthewLeeHan <mlh6fc@virginia.edu>
2018-07-10 19:33:03 -0400

    Added facebook integration into Events page, used their Page plug-in tool, had to load in some Javascript SDK from them.

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,15 +1,43 @@
 <template>
     <div class="events">
-        <h1 class="header">Events</h1>
+        <div class="container">
+            <h1 class="header">Events</h1>
+            
+            <div class="card-bg fb-page-holder">
+                <div class="fb-page" data-href="https://www.facebook.com/ksaatuva/" data-tabs="timeline, events" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/ksaatuva/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/ksaatuva/">Korean Student Association at UVA</a></blockquote></div>
+            </div>
+            <div class="card-bg more-info">
+                <h3>First General Body Meeting TBD</h3>
+            </div>
+
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
     .events{
-        .header{
+        .container{
             text-align: center;
-            font-size: 45px;
-            margin-top: 40px;
+            .header{
+                text-align: center;
+                font-size: 45px;
+                margin-top: 40px;
+            }
+            .fb-page-holder{
+                display: inline-block;
+            }
+            .card-bg{
+                width: fit-content;
+                height: fit-content;
+                margin: 40px 0 0 0;
+            }
+            .more-info{
+                display: inline-block;
+                width: fit-content;
+                height: fit-content;
+                vertical-align: top;
+                margin-left: 20px;
+            }
         }
     }
 </style>
--- src/views/Expo.vue
+++ src/views/Expo.vue
@@ -1,5 +1,16 @@
 <template>
-    <div class="expo">
-        temp
+    <div class="expo container">
+        <h1 class="header">Expo</h1>
+        
     </div>
 </template>
+
+<style lang="scss" scoped>
+    .expo{
+        .header{
+            text-align: center;
+            font-size: 45px;
+            margin-top: 40px;
+        }
+    }
+</style>
