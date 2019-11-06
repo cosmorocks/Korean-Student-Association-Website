e3801bdeb4175e8d229722a03f20696272e4748b
vyoung98 <val.young98@gmail.com>
2019-11-06 00:57:06 -0500

    AHHHHH

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -2,8 +2,8 @@
     <div class="events">
         <div class="container my-container">
             <h1 class="header">Events</h1>
-            <Event class="event-item" image="smores.jpg" title="Smores Night with JC and ISA" description="Come out to eat some s'mores and meet members from other CIOs!" month="Nov" day="10th" time="7pm" location="Lawn Room 25"></Event>
-            <Event class="event-item" image="first-gen-meeting.jpg" title="Spring General Meeting" description="KSA's spring general body meeting! Come by to learn more about upcoming KSA events, watch our K-EXPO dance previews, and make new friends! See you there!" month="Jan" day="18th" time="6pm" location="Runk Green Room"></Event>
+            <Event class="event-item" image="first-gen-meeting.jpg" title="Spring General Meeting!" description="KSA's spring general body meeting! Come by to learn more about upcoming KSA events, watch our K-EXPO dance previews, and make new friends! See you there!" month="Jan" day="18th" time="6pm" location="Runk Green Room"></Event>
+            <Event class="event-item" title="More to come soon!" description="Visit our facebook page to keep up with all of our events!" month="TBD" day="tbd" time="tbd" location="tbd" image="logo.png"></Event>
             <Event class="event-item" title="More to come soon!" description="Visit our facebook page to keep up with all of our events!" month="TBD" day="tbd" time="tbd" location="tbd" image="logo.png"></Event>
         </div>
         <Footer class="footer"></Footer>
