5473a5d876de33db54a5b74255360af715f974dc
vyoung98 <val.young98@gmail.com>
2019-11-06 01:31:04 -0500

    smores stuff

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
                 <router-link class="nav-link" tag="a" to="/events">Events</router-link>
               </div>
               <div class="nav-item mr-5 k-expo-link">
-                <router-link class="nav-link" tag="a" to="/expo">K-Expo</router-link>
+                <router-link class="nav-link" tag="a" to="/expo">K-EXPO</router-link>
               </div>
             </div>
           </div>
